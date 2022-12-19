c4fa8953d55a36079fbd608b8b041249834be3cf
Erwan Jesson Ripoll <70474271+cto-leaps@users.noreply.github.com>
2022-12-19 09:27:53 +0100

    Fixed TicketCard styling

--- lib/features/tickets/widgets/ticket_card.dart
+++ lib/features/tickets/widgets/ticket_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:helpdesk/design_system/atoms/primary_button.dart';
+import 'package:helpdesk/design_system/constants.dart';
 import 'package:helpdesk/features/authentication/authentication.dart';
 import 'package:helpdesk/repositories/ticket_repository/models/ticket.dart';
 
@@ -18,23 +19,27 @@ class TicketCard extends StatelessWidget {
       builder: (context, authState) {
         return Card(
           elevation: 10.0,
-          child: Column(
-            children: [
-              Text(
-                "Ticket ID# ${ticket.id}",
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Text(
-                ticket.title,
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              Text(ticket.description ?? ''),
-              if (authState.user!.id == ticket.owner)
-                PrimaryButton(
-                  child: const Text("Edit"),
-                  onPressed: () {},
-                )
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(kPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Ticket ID# ${ticket.id}",
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                Text(
+                  ticket.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(ticket.description ?? ''),
+                if (authState.user!.id == ticket.owner)
+                  PrimaryButton(
+                    child: const Text("Edit"),
+                    onPressed: () {},
+                  )
+              ],
+            ),
           ),
         );
       },
